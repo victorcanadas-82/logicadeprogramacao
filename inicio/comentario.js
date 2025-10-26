bc44ebf0cc7ac8e66b66db3b9209136ade5c8a3b
victorcanadas <victorcanadas@hotmail.com>
2025-10-26 15:47:46 -0300

    comentarios

---
+++ inicio/comentario.js
@@ -0,0 +1,10 @@
+// console.log é usado para imprimir mensagens no console
+console.log('Passo #03');
+console.log('Passo #02'); // mais um exemplo de console.log
+// console.log('Passo #01');
+
+/*
+    Esse é um comentário de múltiplas linhas
+    Ele pode ser usado para explicar trechos de código
+    ou desabilitar blocos inteiros de código
+*/
