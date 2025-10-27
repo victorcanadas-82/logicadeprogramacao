3473ab9f589bd31db756ec34c757c3c4a4134587
victorcanadas <victorcanadas@hotmail.com>
2025-10-27 17:04:26 -0300

    dados/variaveis

---
+++ inicio/dados_2.js
@@ -0,0 +1,15 @@
+let preco = 19.90
+let desconto = 0.4 
+
+console.log(preco * (1 - desconto));
+
+let precoComDesconto = preco * (1 - desconto)
+console.log(precoComDesconto);  
+
+
+let nome = "Caderno" // String -> sequencia de caracteres
+let categoria = "Papelaria"
+console.log("Produto: " + nome + ", Categoria: " + categoria);
+
+console.log(1 + 1);
+console.log("1" + 1);
