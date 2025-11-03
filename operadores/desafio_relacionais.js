a7ae15afa2b793dee4a34eb7a80ac2d1e20a348c
victorcanadas <victorcanadas@hotmail.com>
2025-11-03 16:52:39 -0300

    operadores lógicos

---
+++ operadores/desafio_relacionais.js
@@ -0,0 +1,17 @@
+let a = 1;
+let b = 2;
+
+// Encontre 5 expressoes relacionais que resultem em true
+console.log(a < b);    
+console.log(a <= b);
+console.log(b > a);
+console.log(b >= a);
+console.log(a != b);
+
+// Encontre 5 expressoes relacionais que resultem em false
+console.log(a > b);    
+console.log(a >= b);
+console.log(b < a);
+console.log(b <= a);
+console.log(a == b);
+
---
+++ operadores/relacionais_2.js
@@ -0,0 +1,12 @@
+let a = 1
+let b = "1"
+let c = "1"
+
+console.log(a == b);    // true - comparação de valor
+console.log(a === b);   // false - comparação de valor e tipo
+console.log(a === b);   // false - comparação de valor e tipo
+console.log(b === c);   // true - comparação de valor e tipo
+
+console.log(a != b);    // false - comparação de valor
+console.log(a !== b);   // true - comparação de valor e tipo
+console.log(b !== c);   // false - comparação de valor e tipo
